a8bdce7a4aa201102d2f77dbb41765eb4248a1a8
Cgneily <Chris.neily@gmail.com>
2024-10-14 02:56:46 -0600

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Christopher Neily on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
